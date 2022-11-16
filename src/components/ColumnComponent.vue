015459b9bff3c0b8a19ff9b65a1dbe8a3dcd132e
Ksenia Stupina <k.stupina@okenit.ru>
2022-11-16 15:45:15 +0300

    init copy text

--- src/components/ColumnComponent.vue
+++ src/components/ColumnComponent.vue
@@ -3,8 +3,11 @@
       :style="background"
       class="column"
   >
-    <h2 :class="['text', textColor]">
-      {{colorHex}}
+    <h2
+        :class="['text', textColor]"
+        @click="copy"
+    >
+      {{ text }}
     </h2>
 
     <button
@@ -31,6 +34,7 @@ export default {
   data() {
     return {
       isClose: true,
+      isCopy: false
     }
   },
   props: {
@@ -44,55 +48,66 @@ export default {
     },
   },
   computed: {
-    background () {
+    background() {
       return 'background: ' + this.$props.backgroundColor;
     },
-    textColor () {
+    textColor() {
       const luminance = chroma(this.$props.backgroundColor).luminance();
       return luminance > 0.5 ? 'textBlack' : 'textWhite';
+    },
+    text() {
+      return this.$data.isCopy ? 'copy' : this.$props.colorHex;
     }
   },
+  methods: {
+    copy() {
+      this.$data.isCopy = true;
+      navigator.clipboard.writeText(this.$props.colorHex)
+      setTimeout(() => this.$data.isCopy = false, 800)
+    },
+
+  }
 }
 </script>
 
 <style scoped>
-  .column {
-    height: 100vh;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-  }
+.column {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
 
-  .text {
-    font-size: 20px;
-    padding: 12px;
-    border-radius: 8px;
-    transition: background-color 400ms;
-  }
+.text {
+  font-size: 20px;
+  padding: 12px;
+  border-radius: 8px;
+  transition: background-color 400ms;
+}
 
-  .textWhite {
-    color: #fff;
-  }
+.textWhite {
+  color: #fff;
+}
 
-  .textBlack {
-    color: #000;
-  }
+.textBlack {
+  color: #000;
+}
 
-  .button {
-    background-color: transparent;
-    padding: 12px;
-    border-radius: 8px;
-    border: none;
-    outline: none;
-    transition: background-color 400ms;
-  }
+.button {
+  background-color: transparent;
+  padding: 12px;
+  border-radius: 8px;
+  border: none;
+  outline: none;
+  transition: background-color 400ms;
+}
 
-  .button:hover,
-  .text:hover {
-    background-color: #0000001A;
-    cursor: pointer;
-  }
+.button:hover,
+.text:hover {
+  background-color: #0000001A;
+  cursor: pointer;
+}
 
 </style>
