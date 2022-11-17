5b8f789c172b527f562f119832a8971de2f48a08
Ksenia Stupina <k.stupina@okenit.ru>
2022-11-17 14:30:19 +0300

    add main hint

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="container">
+    <MainHint
+        v-if="isHintVisible"
+        :colorHex="middleColumnColor"
+    />
     <ColumnComponent
         v-for="(color, i) in colors"
         :key="i"
@@ -13,28 +17,34 @@
 <script>
 import { initRandomColorsArray } from "@/helpers/initRandomColorsArray.js";
 import { generateRandomColor} from "@/helpers/generateRandomColor.js";
-import ColumnComponent from "@/components/ColumnComponent.vue";
 import {updateColorsHash} from "@/helpers/updateColorsHash.js";
+import ColumnComponent from "@/components/ColumnComponent.vue";
+import MainHint from "@/components/MainHint.vue";
 
 export default {
   name: 'App',
   data() {
     return {
+      isHintVisible: true,
+      middleColumnColor: undefined,
       columnQwt: 5,
       colors: [],
     }
   },
   components: {
     ColumnComponent,
+    MainHint,
   },
   beforeMount() {
     this.$data.colors = initRandomColorsArray(this.$data.columnQwt);
+    this.getMiddleColumnColor();
     updateColorsHash(this.$data.colors);
 
     document.addEventListener('keydown', (e) => {
       e.preventDefault();
 
       if (e.code.toLowerCase() === 'space') {
+        this.$data.isHintVisible = false;
         this.$data.colors.forEach(col => {
             if(!col.isBlocked) {
               col.color = generateRandomColor();
@@ -44,6 +54,11 @@ export default {
       }
     })
   },
+  created() {
+    setTimeout(() => {
+      this.$data.isHintVisible = false;
+    }, 3000);
+  },
   methods: {
     onDoNotChange (index) {
       const oldColor = this.$data.colors[index];
@@ -52,6 +67,10 @@ export default {
         isBlocked: !oldColor.isBlocked,
       })
     },
+    getMiddleColumnColor() {
+      const index = Math.floor(this.$data.columnQwt / 2);
+      this.$data.middleColumnColor = this.$data.colors[index].color;
+    }
   },
 }
 </script>
@@ -62,6 +81,7 @@ export default {
     font-family: 'Roboto', sans-serif;
   }
   .container {
+    position: relative;
     display: flex;
     width: 100%;
     height: 100%;
---
+++ src/components/MainHint.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :class="style">
+    PRESS SPACE
+  </div>
+</template>
+
+<script>
+
+import chroma from "chroma-js";
+
+export default {
+  name: "MainHint",
+  props: {
+    colorHex: {
+      type: String,
+      required: true,
+    }
+  },
+  computed: {
+    style() {
+      const luminance = chroma(this.$props.colorHex).luminance();
+      return luminance > 0.5 ? ['textBlack', 'hint'] : ['textWhite', 'hint'];
+    },
+  }
+}
+</script>
+
+<style scoped>
+.hint {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #0000001A;
+  padding: 12px;
+  border-radius: 8px;
+}
+
+.textWhite {
+  color: #fff;
+}
+
+.textBlack {
+  color: #000;
+}
+
+</style>
